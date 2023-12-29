87d95a418538f130f949b508d30ea0ef8a36271f
“Usman <mgsukhan@gmail.com>
2023-12-30 01:06:03 +0300

    commiting

---
+++ main.py
@@ -0,0 +1,69 @@
+from dotenv import load_dotenv
+import googleapiclient.errors
+import googleapiclient.discovery
+import os
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+tokenizer = AutoTokenizer.from_pretrained(
+    'nlptown/bert-base-multilingual-uncased-sentiment')
+
+model = AutoModelForSequenceClassification.from_pretrained(
+    'nlptown/bert-base-multilingual-uncased-sentiment')
+
+load_dotenv()
+api_key = os.getenv("API_KEY")
+
+
+def get_comments(youtube, **kwargs):
+    comments = []
+    results = youtube.commentThreads().list(**kwargs).execute()
+
+    while results:
+        for item in results['items']:
+            comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+            comments.append(comment)
+
+        # check if there are more comments
+        if 'nextPageToken' in results:
+            kwargs['pageToken'] = results['nextPageToken']
+            results = youtube.commentThreads().list(**kwargs).execute()
+        else:
+            break
+
+    return comments
+
+
+def commen(video_id, api_key):
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    youtube = googleapiclient.discovery.build(
+        "youtube", "v3", developerKey=api_key)
+
+    comments = get_comments(youtube, part="snippet",
+                            videoId=video_id, textFormat="plainText")
+    return comments
+
+
+def get_video_comments(video_id):
+    return commen(video_id, api_key)
+
+
+def makevideoid(url):
+    video_id = url.split("=")[1].split("&")[0]
+    return video_id
+
+
+def main():
+    video = input("enter the url of the youtube video:")
+    videoid = makevideoid(video)
+    comments = get_video_comments(videoid)
+    length = len(comments)
+    num = 0
+    for i in range(length):
+        tokens = tokenizer.encode(comments[i], return_tensors='pt')
+        result = model(tokens)
+        num += int(torch.argmax(result.logits))+1
+    return (num/length)
+
+
+print(main())
